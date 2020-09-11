1b86ad9a9051c10e2b7d279fd352f231f5697886
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-09-12 00:03:34 +0300

    Initial Commit

---
+++ 1_laba.cpp
@@ -0,0 +1,21 @@
+﻿/* Программa, которая проверяет на четность
+введенное пользователем число.*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+
+    cout << "Введите число: ";
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << n << " - четноею.";
+    else
+        cout << n << " - нечетное.";
+    cout << endl;
+    return 0;
+}
+
